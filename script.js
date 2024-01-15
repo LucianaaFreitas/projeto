97d3d3ff50fdd31db8b9a7da326cf99e1f093da4
luciana <lucianafreitasdev@gmail.com>
2024-01-15 15:49:06 -0300

    adjust for Luciana Freitas

--- script.js
+++ script.js
@@ -8,18 +8,10 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se  tiver com light mode. adicionar a imagem ligth
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/img-luciana-light.png")
   } else {
     //se tiver sem light mode. adicionar a imagem ligth
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/img-luciana.png")
   }
 
-  //mudar a escrita da imagem com light mode
-  // {
-  //img.setAttribute("alt", "Foto de Mayk Brito sorrindo, de óculos de sol e camisa azul ")
-  //} else {
-  //mudar a escrita da imagem sem light mode
-  //img.setAttribute("alt", "Foto de Mayk Brito sorrindo, //de óculos e camiseta preta.")
-
-  //}
 }
